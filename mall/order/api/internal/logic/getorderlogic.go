61959c82218527038500f1af137fc972b997971e
vincent-guo <vincent.weiguo@gmail.com>
2021-10-24 23:47:16 +0800

    update getorderlogic.go

--- mall/order/api/internal/logic/getorderlogic.go
+++ mall/order/api/internal/logic/getorderlogic.go
@@ -2,6 +2,8 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"go-zero-demo/mall/user/rpc/userclient"
 
 	"go-zero-demo/mall/order/api/internal/svc"
 	"go-zero-demo/mall/order/api/internal/types"
@@ -24,7 +26,19 @@ func NewGetOrderLogic(ctx context.Context, svcCtx *svc.ServiceContext) GetOrderL
 }
 
 func (l *GetOrderLogic) GetOrder(req types.OrderReq) (*types.OrderReply, error) {
-	// todo: add your logic here and delete this line
+	user, err := l.svcCtx.UserRpc.GetUser(l.ctx, &userclient.IdRequest{
+		Id: "1",
+	})
+	if err != nil {
+		return nil, err
+	}
 
-	return &types.OrderReply{}, nil
+	if user.Name != "test" {
+		return nil, errors.New("用户不存在")
+	}
+
+	return &types.OrderReply{
+		Id: req.Id,
+		Name: "test order",
+	}, nil
 }
