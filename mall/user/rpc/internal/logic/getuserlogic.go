dd6868444843ec30bb7cde954eb755615ba741c0
vincent-guo <vincent.weiguo@gmail.com>
2021-10-24 22:54:28 +0800

    generate rpc

---
+++ mall/user/rpc/internal/logic/getuserlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"go-zero-demo/mall/user/rpc/internal/svc"
+	"go-zero-demo/mall/user/rpc/user"
+
+	"github.com/tal-tech/go-zero/core/logx"
+)
+
+type GetUserLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewGetUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetUserLogic {
+	return &GetUserLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *GetUserLogic) GetUser(in *user.IdRequest) (*user.UserResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &user.UserResponse{}, nil
+}
