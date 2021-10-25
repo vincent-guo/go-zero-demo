6a31f4d3b119eee7ebf6e6c8d1d0394be1ab1ebf
vincent-guo <vincent.weiguo@gmail.com>
2021-10-25 22:27:50 +0800

    update servicecontext.go

--- book/service/user/rpc/internal/svc/servicecontext.go
+++ book/service/user/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,20 @@
 package svc
 
-import "go-zero-demo/book/service/user/rpc/internal/config"
+import (
+	"github.com/tal-tech/go-zero/core/stores/sqlx"
+	"go-zero-demo/book/service/user/model"
+	"go-zero-demo/book/service/user/rpc/internal/config"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config    config.Config
+	UserModel model.UserModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	conn := sqlx.NewMysql(c.Mysql.Datasource)
 	return &ServiceContext{
 		Config: c,
+		UserModel: model.NewUserModel(conn, c.CacheRedis),
 	}
 }
