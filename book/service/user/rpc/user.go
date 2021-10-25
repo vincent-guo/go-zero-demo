25aa5cac5e763e5b9ac5c4cbd8c235f552a22dac
vincent-guo <vincent.weiguo@gmail.com>
2021-10-25 22:12:57 +0800

    generate rpc

---
+++ book/service/user/rpc/user.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"go-zero-demo/book/service/user/rpc/internal/config"
+	"go-zero-demo/book/service/user/rpc/internal/server"
+	"go-zero-demo/book/service/user/rpc/internal/svc"
+	"go-zero-demo/book/service/user/rpc/user"
+
+	"github.com/tal-tech/go-zero/core/conf"
+	"github.com/tal-tech/go-zero/core/service"
+	"github.com/tal-tech/go-zero/zrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var configFile = flag.String("f", "etc/user.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+	srv := server.NewUserServer(ctx)
+
+	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		user.RegisterUserServer(grpcServer, srv)
+
+		switch c.Mode {
+		case service.DevMode, service.TestMode:
+			reflection.Register(grpcServer)
+		default:
+		}
+
+	})
+	defer s.Stop()
+
+	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	s.Start()
+}
