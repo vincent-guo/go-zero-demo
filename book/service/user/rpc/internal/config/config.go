c409d3ace1dd9e342fc87ad41ec9d735d1d83424
vincent-guo <vincent.weiguo@gmail.com>
2021-10-25 22:27:18 +0800

    update config.go

--- book/service/user/rpc/internal/config/config.go
+++ book/service/user/rpc/internal/config/config.go
@@ -1,7 +1,16 @@
 package config
 
-import "github.com/tal-tech/go-zero/zrpc"
+import (
+	"github.com/tal-tech/go-zero/core/stores/cache"
+	"github.com/tal-tech/go-zero/zrpc"
+)
 
 type Config struct {
 	zrpc.RpcServerConf
+
+	Mysql struct{
+		Datasource string
+	}
+
+	CacheRedis cache.CacheConf
 }
