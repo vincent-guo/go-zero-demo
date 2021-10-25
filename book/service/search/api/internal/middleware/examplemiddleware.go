83479713f567f6a8dab06618629ac938443d9b47
vincent-guo <vincent.weiguo@gmail.com>
2021-10-25 22:08:17 +0800

    update examplemiddleware.go

--- book/service/search/api/internal/middleware/examplemiddleware.go
+++ book/service/search/api/internal/middleware/examplemiddleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"github.com/tal-tech/go-zero/core/logx"
 	"net/http"
 )
 
@@ -13,7 +14,7 @@ func NewExampleMiddleware() *ExampleMiddleware {
 
 func (m *ExampleMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// TODO generate middleware implement function, delete after code implementation
+		logx.Info("example middle")
 
 		// Passthrough to next handler if need
 		next(w, r)
